6e0c4b065efc8ed59eb3c903cf3c65cd6f7395b4
robhefty <3984882+robhefty@users.noreply.github.com>
2018-01-28 00:42:55 -0600

    SQL select history page

---
+++ HeftyTwitterApp/History.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="History.aspx.cs" Inherits="HeftyTwitterApp.History" %>
+
+    <div class="jumbotron">
+        <h1>Hefty Twitter App</h1>
+        <p>Compare the popularity of two terms or phrases on Twitter</p>
+    </div>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+
+    <asp:PlaceHolder ID="PlaceHolder1" runat="server" />
+</body>
+</html>
---
+++ HeftyTwitterApp/History.aspx.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using System.Web.UI.WebControls;
+
+namespace HeftyTwitterApp
+{
+    public partial class History : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.IsPostBack)
+            {
+                //Populating a DataTable from database.
+                DataTable dt = this.GetData();
+
+                //Building an HTML string.
+                StringBuilder html = new StringBuilder();
+
+                //Table start.
+                html.Append("<table border = '1'>");
+
+                //Building the Header row.
+                html.Append("<tr>");
+                foreach (DataColumn column in dt.Columns)
+                {
+                    html.Append("<th>");
+                    html.Append(column.ColumnName);
+                    html.Append("</th>");
+                }
+                html.Append("</tr>");
+
+                //Building the Data rows.
+                foreach (DataRow row in dt.Rows)
+                {
+                    html.Append("<tr>");
+                    foreach (DataColumn column in dt.Columns)
+                    {
+                        html.Append("<td>");
+                        html.Append(row[column.ColumnName]);
+                        html.Append("</td>");
+                    }
+                    html.Append("</tr>");
+                }
+
+                //Table end.
+                html.Append("</table>");
+
+                //Append the HTML string to Placeholder.
+                PlaceHolder1.Controls.Add(new Literal { Text = html.ToString() });
+            }
+        }
+
+        private DataTable GetData()
+        {
+            string connstr = ConfigurationManager.ConnectionStrings["ConnStr"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(connstr))
+            {
+                using (SqlCommand cmd = new SqlCommand("SELECT search1, tweets1, search2, tweets2, seconds, dtm from searches order by dtm desc;"))
+                {
+                    using (SqlDataAdapter sda = new SqlDataAdapter())
+                    {
+                        cmd.Connection = con;
+                        sda.SelectCommand = cmd;
+                        using (DataTable dt = new DataTable())
+                        {
+                            sda.Fill(dt);
+                            return dt;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
